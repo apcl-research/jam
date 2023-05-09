54efa55d271f841e0d67873eafc32b45cd8dd5df
Chia-Yi Su <csu3@nd.edu>
2023-05-09 17:49:19 +0000

    add script for download files

---
+++ data/download.py
@@ -0,0 +1,16 @@
+from huggingface_hub import hf_hub_download, snapshot_download
+import fire
+
+def main(download_file:bool = False,
+        repo_id:str = 'apcl/java52m', # the repo that you want to download the file
+        filename:str='',     # if you only want to download a single file, specify the name of that file
+        local_dir:str='test' # the local directory for the dataset file
+        ):
+    if(not download_file):
+        snapshot_download(repo_id=repo_id, local_dir=local_dir, repo_type="dataset")
+    else:
+        snapshot_download(repo_id=repo_id, filename=filename, local_dir = local_dir, repo_type="dataset")
+
+if __name__=='__main__':
+    fire.Fire(main)
+
