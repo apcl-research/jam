562fd7f53174c804d5da40c34f98b688f6277fed
Chia-Yi Su <csu3@nd.edu>
2023-05-09 14:21:49 +0000

    fix

--- data/jam_so13m/dedup_stackoverflow.py
+++ data/jam_so13m/dedup_stackoverflow.py
@@ -9,29 +9,31 @@ import fire
 
 
 
-def main(stackoverflow_textfilename_list = '/sorna/datasets/jam_so13m/stackoverflow_txtfiles.pkl',
-        fundats_filename = '/sorna/datasets/jam_jm52m/fundats-j1.pkl',
-        stackoverflow_textdata_filename = '/sorna/datasets/jam_so13m/jam_so13m.pkl',
-        out_filename = 'dedup_testfids_m_th_5.txt',
-        threshold = 0.50 # control the percentage of similarity
+def main(stackoverflow_textfilename_list:str = '/sorna/datasets/jam_so13m/stackoverflow_txtfiles.pkl',
+        fundats_filename:str = '/sorna/datasets/jam_jm52m/fundats-j1.pkl',
+        stackoverflow_textdata_filename:str = '/sorna/datasets/jam_so13m/jam_so13m.pkl',
+        out_filename:str = 'dedup_testfids_m_th_5.txt',
+        threshold:float = 0.50, # control the percentage of similarity
+        test_filename:str='/nfs/projects/funcom/data/javastmt_fc/output/tdats.test',
+        lsh_outdir:str='stackoverflow/so_lsh_parts'
         ):
     stackoverflow_file = open(stackoverflow_textfilename_list,'rb')
     files_a = pickle.load(stackoverflow_file)
 
-    numfiles_a = int(len(files_a) / 2)
+    numfiles_a = len(files_a)
     nfsplit = int(numfiles_a / 100)
 
-    files_a_split = [files_a[i:i+nfsplit] for i in range(numfiles_a, len(files_a), nfsplit)]
+    files_a_split = [files_a[i:i+nfsplit] for i in range(0, len(files_a), nfsplit)]
 
     fundats = pickle.load(open(fundats_filename, 'rb'))
     funids = list(fundats.keys())
     stackoverflow_dats = pickle.load(open( stackoverflow_textdata_filename , 'rb'))
     for partnum, files_a_part in enumerate(files_a_split):
-
-        if os.path.isfile(f'so_lsh_parts/so_lsh_p{partnum}.pkl'):
-            lsh = pickle.load(open(f'so_lsh_parts/so_lsh_p{partnum}.pkl', 'rb'))
+        lsh_path = lsh_outdir + f'/so_lsh_p{partnum}.pkl'
+        if os.path.isfile(lsh_path):
+            lsh = pickle.load(open(lsh_path, 'rb'))
         else:
-            lsh = MinHashLSH(threshold=0.50, num_perm=128)
+            lsh = MinHashLSH(threshold=threshold, num_perm=128)
         # Generate MinHash signatures for all files in folder A
             for filename in tqdm(files_a_part, desc=f'processing so folder part {partnum}'):
                 if filename.endswith('.txt'):
@@ -44,24 +46,25 @@ def main(stackoverflow_textfilename_list = '/sorna/datasets/jam_so13m/stackoverf
                     # Add the signature to the LSH index
                     lsh.insert(filename, minhash)
 
-            with open(f'stackoverflow/so_lsh_parts/so_lsh_m_p{partnum}.pkl', 'wb') as f:
+            with open(lsh_path, 'wb') as f:
                 pickle.dump(lsh, f)
 
 
         outf = open(out_filename, 'a')
+        with open(test_filename, 'r') as fb:
+            for line in tqdm(fb):
+                (fid, tdat) = line.split('<SEP>')
+                fid = int(fid)
 
-        for funid in tqdm(funids):
-            fid = int(funid)
+                tdat = fundats[fid]
 
-            tdat = fundats[fid]
+                minhash = MinHash(num_perm=128)
+                for word in tdat.split(' '):
+                    minhash.update(word.encode('utf-8'))
+                matches = lsh.query(minhash)
 
-            minhash = MinHash(num_perm=128)
-            for word in tdat.split(' '):
-                minhash.update(word.encode('utf-8'))
-            matches = lsh.query(minhash)
-
-            if len(matches) != 0:
-                print(f'{fid}\t{matches}', file=outf)
+                if len(matches) != 0:
+                    print(f'{fid}\t{matches}', file=outf)
 
         outf.flush()
         outf.close()
