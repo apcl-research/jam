8f399128afd33404391c22d716c2b60af2736829
Chia-Yi Su <csu3@nd.edu>
2023-05-10 22:56:09 +0000

    refactor

--- download_extract_file.py
+++ download_extract_file.py
@@ -4,9 +4,10 @@ import fire
 
 
 def main(repo_id:str='apcl/funcom-java-long', 
-        local_dir:str ='./'):
-    hf_hub_download(repo_id=repo_id, local_dir=local_dir, filename = 'funcom_test.tar.gz', repo_type='dataset' )
-    fname = local_dir + 'funcom_test.tar.gz'
+        local_dir:str ='./',
+        filename:str='funcom_test.tar.gz'):
+    hf_hub_download(repo_id=repo_id, local_dir=local_dir, filename = filename, repo_type='dataset' )
+    fname = local_dir + filename
     if fname.endswith("tar.gz"):
         tar = tarfile.open(fname, "r:gz")
         tar.extractall()
