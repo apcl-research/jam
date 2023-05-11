93c2acb004a52f5b8661deec601208170b9352b5
Chia-Yi Su <csu3@nd.edu>
2023-05-11 23:56:18 +0000

    remove flag

--- download.py
+++ download.py
@@ -1,13 +1,12 @@
 from huggingface_hub import hf_hub_download, snapshot_download
 import fire
 
-def main(download_file:bool = False,
-        repo_id:str = 'apcl/java52m', # the repo that you want to download the file
+def main(repo_id:str = 'apcl/java52m', # the repo that you want to download the file
         filename:str='',     # if you only want to download a single file, specify the name of that file
         local_dir:str='./data', # the local directory for the dataset file
         repo_type:str='None' # set to dataset if you want to download file from dataset repo
         ):
-    if(not download_file):
+    if(filename==''):
         snapshot_download(repo_id=repo_id, local_dir=local_dir, repo_type=repo_type)
     else:
         hf_hub_download(repo_id=repo_id, filename=filename, local_dir = local_dir, repo_type=repo_type)
