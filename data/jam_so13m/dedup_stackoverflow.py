a1025a66a07defb7e25fc8200d9f26301fef7eb5
Chia-Yi <csu3@nd.edu>
2023-05-12 00:33:43 +0000

    fix bugs

--- data/jam_so13m/dedup_stackoverflow.py
+++ data/jam_so13m/dedup_stackoverflow.py
@@ -58,7 +58,7 @@ def main(test_filename:str= '/nfs/projects/funcom/data/javastmt_fc/output/tdats.
          partend:int=100
 
         ):
-    if(os.path.exists(lsh_dir)):
+    if(not os.path.exists(lsh_dir)):
         os.mkdir(lsh_dir)
     stackoverflow_file = open(stackoverflow_text_id_filename,'rb')
     files_a = pickle.load(stackoverflow_file)
